984e71ededbc419d90e5fb2595cd702ebca517d6
mr.danielcosta <31771918+danielhrc@users.noreply.github.com>
2023-05-30 20:14:31 -0300

    Update getTokensFromProprietarios.go

--- chaincode/txdefs/getTokensFromProprietarios.go
+++ chaincode/txdefs/getTokensFromProprietarios.go
@@ -28,23 +28,52 @@ var GetNumberOfTokensFromProprietario = tx.Transaction{
 	},
 	Routine: func(stub *sw.StubWrapper, req map[string]interface{}) ([]byte, errors.ICCError) {
 
-		idProprietario, _ := req["proprietario"].(assets.Key)
+		idProprietario, ok := req["proprietario"].(assets.Key)
 
-		// Prepare query
+		if !ok {
+			return nil, errors.WrapError(nil, "Parametro proprietario deve ser um ativo.")
+		}
+
+		proprietarioAsset, errKey := idProprietario.Get(stub)
+	
+		
+		if errKey != nil {
+			return nil, errors.WrapError(errKey, "Falha ao obter proprietario.")
+		}
+		proprietarioMap := (map[string]interface{})(*proprietarioAsset)
+
+		searchProp := make(map[string]interface{})
+		searchProp["@assetType"] = "proprietario"
+		searchProp["@key"] = proprietarioMap["@key"]
+
 		query := map[string]interface{}{
 			"selector": map[string]interface{}{
 				"@assetType":   "token",
-				"proprietario": idProprietario,
+				"proprietario": searchProp,
 			},
 		}
 
 		var err error
 		response, err := assets.Search(stub, query, "", true)
 		if err != nil {
-			return nil, errors.WrapErrorWithStatus(err, "error searching  proprietrio", 500)
+			return nil, errors.WrapErrorWithStatus(err, "Erro searching proprietário.", 500)
 		}
 
-		responseJSON, err := json.Marshal(response)
+		tokens := response.Result
+
+		var quantidade float64 = 0
+
+		for i := 0; i < len(tokens); i++ {
+			if !tokens[i]["burned"].(bool) {
+				quantidade = quantidade + tokens[i]["quantidade"].(float64)
+			}
+		}
+
+		balance := make(map[string]interface{})
+		
+		balance["quantidade"] = quantidade
+		
+		responseJSON, err := json.Marshal(balance)
 		if err != nil {
 			return nil, errors.WrapErrorWithStatus(err, "error marshaling response", 500)
 		}
